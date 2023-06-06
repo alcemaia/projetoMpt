3487e4b02b13f45bfdd756462c6b61c75d06c5b7
AlceMaia <alceniziamaia@gmail.com>
2023-06-06 00:36:47 -0300

    alterações de imagens nas propostas e cor dos titulos no zig zag

--- components/proposta.tsx
+++ components/proposta.tsx
@@ -4,7 +4,7 @@
 import Image from 'next/image'
 
 export default function Proposta(props: any) {
-  const {points, topTitle, title, description, img, isLeft, itemA, itemB, itemC,itemD} = props
+  const {points, title, img, isLeft,} = props
   const side = isLeft
   return (
 <>
@@ -16,7 +16,7 @@ export default function Proposta(props: any) {
       <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-5 lg:col-span-6 mb-8 md:mb-0 rtl" data-aos="fade-up">
         <Image className="max-w-full mx-auto md:max-w-none h-auto" src={img} width={540} height={405} alt="Features 02" />
       </div>
-      <div className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
+      <div className="max-w-xl text-vermelho-900 md:max-w-none md:w-full mx-auto md:col-span-7 lg:col-span-6" data-aos="fade-left">
         <div className="md:pl-4 lg:pl-12 xl:pl-16">
           <h3 className="h3 mb-3">{title}</h3>
           <ul className="text-lg text-gray-400 -mb-2">
