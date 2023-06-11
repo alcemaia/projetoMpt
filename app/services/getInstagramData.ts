8dec2c4e70ad4c7a14ff5f2a1a4110531ac00687
DESKTOP-5SMQO9E\izich <ivan.name Ivan>
2023-06-10 21:20:03 -0300

    instagram image post filter

--- app/services/getInstagramData.ts
+++ app/services/getInstagramData.ts
@@ -2,23 +2,37 @@ import { IgPostProps } from "@/types/api";
 
 export const apiInstagram = async () => {
   let response
+  
+  const  limitarString = (texto: string, limite: number): string => {
+  if (texto.length <= limite) {
+    return texto;
+  }
+  return texto.substr(0, limite) + '...'; 
+  }
+
   const accessToken = process.env.NEXT_PUBLIC_IG_TOKEN
   const fields = 'media_url,media_type,permalink,id,caption'
   const url = `https://graph.instagram.com/me/media?fields=${fields}&access_token=${accessToken}`
   
+  
   const receivedData = await fetch(url).then(r=>r.json())
   const data = receivedData.data
   if(data !== undefined) {
-    // RESOLVE OS DADOS. 
-    const lastSix = receivedData.data.filter((post:any, index: number) => {
+    const filterPosts = receivedData.data.filter((post:any, index: number) => {
+        if (post.media_type !== 'IMAGE') return false
+        return true
+    })
+    const lastSix = filterPosts?.filter((post:any, index: number) => {
         if(index <= 5) return true
         return false
     })
     if(lastSix !== undefined) {
       const postData: IgPostProps = lastSix.map((post: IgPostProps) => {
+
+        const newTitle = limitarString(post.caption, 30)
         const returnPost: IgPostProps = {
           id: post.id, 
-          title: post.caption,
+          title: newTitle,
           description: 'Está é a descrição da proposta, post do Instagram ou vídeo.', 
           media_type: post.media_type,
           media_url: post.media_url,
--- components/instagram.tsx
+++ components/instagram.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link'
 import { InstagramPageProps } from '@/types/api';
 
 export default function Instagram(props: InstagramPageProps) {
-  const { title, description, igPosts } = props
+  const { title, igPosts } = props
 
   return (
     <section>
@@ -13,7 +13,7 @@ export default function Instagram(props: InstagramPageProps) {
         <div className="py-12 md:py-20">
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">{title}</h2>
-            <p className="text-xl text-gray-400">{description}</p>
+            {/* <p className="text-xl text-gray-400">{description}</p> */}
           </div>
           <div className="max-w-sm mx-auto grid gap-8 md:grid-cols-2 lg:grid-cols-3 lg:gap-16 items-start md:max-w-2xl lg:max-w-none" data-aos-id-blocks>
             {igPosts?.map((card: any) => {
@@ -26,7 +26,6 @@ export default function Instagram(props: InstagramPageProps) {
                   <Card
                     key={card.media_url}
                     title={card.title}
-                    description={card.description}
                     img={card.media_url}
                   />  
                 </Link>
