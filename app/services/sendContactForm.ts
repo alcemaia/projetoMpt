b60c51545d36a4abc94a0c1cb01cd2907571fd24
AlceMaia <alceniziamaia@gmail.com>
2023-06-18 20:54:21 -0300

    ajuste na url do form

--- app/services/sendContactForm.ts
+++ app/services/sendContactForm.ts
@@ -1,9 +1,8 @@
 import { InitValuesForm } from "@/types/api"
 export const sendContactForm = async (data: InitValuesForm) => {
     let response = false
-    const API_SITE = process.env.NEXT_PUBLIC_API_SITE
-    const url = `${API_SITE}/api/hello`
-    const requestData = await fetch(url, {
+    const url = '/api/hello'
+        const requestData = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(data),
         headers: {
